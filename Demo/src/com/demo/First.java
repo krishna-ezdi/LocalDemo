fe35c378c176c5daf2e5b5efb8def7b2986db2ec
krishnamakwana <krishnamakwana@EZDI-LAP0128>
2022-03-02 13:10:00 +0530

    Added message

--- Demo/src/com/demo/First.java
+++ Demo/src/com/demo/First.java
@@ -3,7 +3,7 @@ package com.demo;
 public class First {
 
 	public static void main(String[] args) {
-		System.out.println("Hello there.!");
-		System.out.println("This is Git Demo..! :) ");
+		System.out.println("Hello there. In Master Branch.!");
+		System.out.println("This is Git Demo..!");
 	}
 }
