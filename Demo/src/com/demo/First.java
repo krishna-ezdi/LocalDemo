39c146206842a1dc318dc0f7ea4b2996f6a88c7d
krishnamakwana <krishnamakwana@EZDI-LAP0128>
2022-03-02 14:33:20 +0530

    second branch

--- Demo/src/com/demo/First.java
+++ Demo/src/com/demo/First.java
@@ -3,7 +3,7 @@ package com.demo;
 public class First {
 
 	public static void main(String[] args) {
-		System.out.println("Hello there, In new Branch.!");
+		System.out.println("Hello there, In new Branch..!");
 		System.out.println("This is Git Demo.");
 	}
 }
