d568d8b6a135a7c56cbdc7fbb21f6b5d4dafa4be
krishnamakwana <krishnamakwana@EZDI-LAP0128>
2022-03-04 11:30:18 +0530

    updated file

--- Demo/src/com/demo/First.java
+++ Demo/src/com/demo/First.java
@@ -4,6 +4,6 @@ public class First {
 
 	public static void main(String[] args) {
 		System.out.println("Hello there. In Master Branch...!");
-		System.out.println("This is Git Demo.");
+		System.out.println("This is Git Demo...");
 	}
 }
